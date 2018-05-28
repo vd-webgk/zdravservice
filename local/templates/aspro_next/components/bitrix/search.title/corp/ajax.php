fdba8aab706d02bc64b9e9192132e3159f1d726a
vd@webgk.ru <vd@webgk.ru>
2018-05-28 15:08:28 +0300

    cp5622: button in search.title

--- local/templates/aspro_next/components/bitrix/search.title/corp/ajax.php
+++ local/templates/aspro_next/components/bitrix/search.title/corp/ajax.php
@@ -24,7 +24,7 @@
 								<img src="<?=SITE_TEMPLATE_PATH?>/images/no_photo_small.png" width="38" height="38">
 							<?endif;?>
 						</div>
-						<div class="bx_item_element">
+						<div class="bx_item_element search_add-basket_block">
 							<span><?=$arItem["NAME"]?></span>
 							<div class="price cost prices">
 								<div class="title-search-price">
@@ -58,23 +58,16 @@
 							</div>
                                                        
 						</div>
-                        <div class="to_basket_from_search">
-                            <svg class="basket_in_top_search" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
-                              <defs>
-                                <style>
-                                  .cls-1 {
-                                    fill: #fff;
-                                    fill-rule: evenodd;
-                                  }
-                                </style>
-                              </defs>
-                              <path data-name="Ellipse 2 copy 6" class="cls-1" d="M1507,122l-0.99,1.009L1492,123l-1-1-1-9h-3a0.88,0.88,0,0,1-1-1,1.059,1.059,0,0,1,1.22-1h2.45c0.31,0,.63.006,0.63,0.006a1.272,1.272,0,0,1,1.4.917l0.41,3.077H1507l1,1v1ZM1492.24,117l0.43,3.995h12.69l0.82-4Zm2.27,7.989a3.5,3.5,0,1,1-3.5,3.5A3.495,3.495,0,0,1,1494.51,124.993Zm8.99,0a3.5,3.5,0,1,1-3.49,3.5A3.5,3.5,0,0,1,1503.5,124.993Zm-9,2.006a1.5,1.5,0,1,1-1.5,1.5A1.5,1.5,0,0,1,1494.5,127Zm9,0a1.5,1.5,0,1,1-1.5,1.5A1.5,1.5,0,0,1,1503.5,127Z" transform="translate(-1486 -111)"/>
-                              </svg>
-                        </div>               
+                                      
 						<div style="clear:both;"></div>
 					</div>
                     
 				</a>
+                <div onclick="/*goTohref();*/" class="to_basket_from_search shine">
+                                <object class="svg_basket_title shine" type="image/svg+xml" data="/local/templates/aspro_next/images/svg/Basket_title_search.svg">
+                                    <img src="/local/templates/aspro_next/images/svg/Basket_title_search.svg" class="svg_basket_title"/>
+                                </object>
+                        </div> 
 			<?else:?>
 				<?if($arItem["MODULE_ID"]):?>
 					<a class="bx_item_block others_result" href="<?=$arItem["URL"]?>">
@@ -90,3 +83,15 @@
 		<?endforeach;?>
 	<?endforeach;?>
 </div>
+<script>
+$(document).on('click', function(e){
+    if($(e.target).hasClass('to_basket_from_search'))
+    {
+     console.log(1);
+        
+    } else{
+     console.log(2);      
+    }
+
+});         
+</script>
