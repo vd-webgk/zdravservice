89508dd243fa2f14fa4c79a5e8cacbdf2045c12e
vd@webgk.ru <vd@webgk.ru>
2018-06-13 15:32:52 +0300

    cp5637: add element check

--- local/modules/webgk.main/lib/iblock/catalog_1c/catalog.php
+++ local/modules/webgk.main/lib/iblock/catalog_1c/catalog.php
@@ -8,13 +8,16 @@ class catalog extends Prototype {
         return parent::getInstance();
     }
     public function getXmlIdProperties($getBigDataElements){
-        $explodeBigDataElements = explode(';', $getBigDataElements[0]);
-        $ourAdditionalGetElement = array(
-            "filter" => array('XML_ID' => $explodeBigDataElements),
-            "select" => array("ID"),
-            "cacheTime" => 3600,
-        );
-        $getBigDataAdditionalElements = $this -> getElements($ourAdditionalGetElement);
-        return Tools::getAssocArrItemsKey($getBigDataAdditionalElements, "ID");
+        if(!empty($getBigDataElements)){
+            $explodeBigDataElements = explode(';', $getBigDataElements[0]);
+            $ourAdditionalGetElement = array(
+                "filter" => array('XML_ID' => $explodeBigDataElements),
+                "select" => array("ID"),
+                "cacheTime" => 3600,
+            );
+            $getBigDataAdditionalElements = $this -> getElements($ourAdditionalGetElement);
+            return Tools::getAssocArrItemsKey($getBigDataAdditionalElements, "ID");
+        } else 
+            return false;
     } 
 }
