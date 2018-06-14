6c782a2a7a0b4459e133b26392384e00dca751ce
Ruslan <br@webgk.ru>
2018-06-14 15:42:08 +0300

    normative goods fixes

--- local/modules/webgk.main/lib/normitems.php
+++ local/modules/webgk.main/lib/normitems.php
@@ -8,34 +8,46 @@ use Bitrix\Main\Option;
 use Webgk\Main\Hlblock\Prototype;
 use Webgk\Main\CSV\CSVToArray;
 
-class gettingNormItems {
+/**
+* класс по работе с нормированными товарами, генерация массива из CSV и работа с HL-блоком
+*/
+class normItems {
+    /**
+    * генерация массива из CSV по информации о нормированных товарах
+    * 
+    * @param mixed $csvFileUrl
+    */
     function NormItemsCSVParse($csvFileUrl) {
-        $csvFileArr = CSVToArray::CSVParse($csvFileUrl, array("Item name", "Item quantity"));
+        $csvFileArr = CSVToArray::CSVParse($csvFileUrl, array("item_name", "item_quantity"));
         foreach ($csvFileArr as $importedItemInfo) {
-            gettingNormItems::updatingElementsForHLQuantityBlock($importedItemInfo);
+            normItems::updatingElementsForHLQuantityBlock($importedItemInfo);
         }
     }
-    
+    /**
+    * добавление и обновление элементов HL-блока о количестве нормированных товаров
+    * 
+    * @param mixed $importedItemInfo
+    */
     function updatingElementsForHLQuantityBlock($importedItemInfo) {
-        if (!empty($importedItemInfo["Item name"])) {
+        if (!empty($importedItemInfo["item_name"])) {
             $hlblock = Prototype::getInstance("GoodsQuantity");
             $resultData = $hlblock->getElements(array(
                 "select" => array("*"),
-                "filter" => array("UF_ITEM_ID" => $importedItemInfo["Item name"])
+                "filter" => array("UF_ITEM_ID" => $importedItemInfo["item_name"])
             ));
             if (!empty($resultData)) {
                 foreach ($resultData as $curResult) {
 
                     $result = $hlblock->updateData($curResult["ID"], array(
-                        'UF_ITEM_ID' => $importedItemInfo["Item name"],
-                        'UF_ITEM_QUANTITY' => $importedItemInfo["Item quantity"],
+                        'UF_ITEM_ID' => $importedItemInfo["item_name"],
+                        'UF_ITEM_QUANTITY' => $importedItemInfo["item_quantity"],
                     ));
                 }
             } else {
 
                 $result = $hlblock->addData(array(
-                    'UF_ITEM_ID' => $importedItemInfo["Item name"],
-                    'UF_ITEM_QUANTITY' => $importedItemInfo["Item quantity"],
+                    'UF_ITEM_ID' => $importedItemInfo["item_name"],
+                    'UF_ITEM_QUANTITY' => $importedItemInfo["item_quantity"],
                 ));
             }
             $resultId = $result->getId();
